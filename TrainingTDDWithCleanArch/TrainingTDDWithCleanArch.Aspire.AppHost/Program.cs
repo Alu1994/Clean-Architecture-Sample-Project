0cf6c89df603c7226a481c3da7105ac6ae304dc2
Matheus Veiga <cavalcante.matheusveiga@gmail.com>
2024-11-14 02:10:05 -0300

    Adds .NET Aspire

---
+++ TrainingTDDWithCleanArch/TrainingTDDWithCleanArch.Aspire.AppHost/Program.cs
@@ -0,0 +1,6 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.TrainingTDDWithCleanArch_Presentation_MinimalAPI>("MinimalAPI");
+builder.AddProject<Projects.TrainingTDDWithCleanArch_Presentation_API>("ControllerAPI");
+
+builder.Build().Run();
